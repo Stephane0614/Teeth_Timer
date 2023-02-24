a08823bd51446ddf4883db5940099b443142797e
stephane0614 <nsimba.stephane033@gmail.com>
2023-02-24 02:58:12 +0100

    ionFormOK

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,81 @@
 import { Component } from '@angular/core';
+// import { ViewWillEnter } from '@ionic/angular';
+import { Subscription, interval } from 'rxjs';
+import { Preferences } from '@capacitor/preferences';
+import { ViewWillEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements ViewWillEnter {
+  
+  private subscription?: Subscription;
+  private secondsCounter = 0;
+  private timerON: boolean = false;
+  clock = '00:00:00';
+  userName!: string | null;
+  zone!: string | null;
 
   constructor() {}
+  ionViewWillEnter(): void {
+    this.setValue();
+  }
 
+  private timelaps(secondCount: number): string {
+    const secNum = parseInt(secondCount.toString(), 10);
+    const hours = Math.floor(secNum / 3600);
+    const minutes = Math.floor((secNum - hours * 3600) / 60);
+    const seconds = secNum - hours * 3600 - minutes * 60;
+    const hoursString = hours < 10 ? '0' + hours : hours.toString();
+    const minutesString = minutes < 10 ? '0' + minutes : minutes.toString();
+    const secondsString = seconds < 10 ? '0' + seconds : seconds.toString();
+    this.clock = hoursString + ':' + minutesString + ':' + secondsString;
+    return this.clock;
+  }
+
+  public startTimer() {
+    this.timerON = true;
+    if (!this.subscription) {
+      this.subscription = interval(1000).subscribe(() => {
+        this.secondsCounter++;
+        this.clock = this.timelaps(this.secondsCounter);
+      });
+    }
+  }
+
+  public pauseTimer() {
+    this.timerON = false;
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+      this.subscription = undefined;
+    }
+  }
+
+  public resetTimer() {
+    this.timerON = false;
+    this.subscription?.unsubscribe();
+    this.subscription = undefined;
+    this.secondsCounter = 0;
+    this.clock = '00:00:00';
+  }
+
+  private setValue = () => {
+    const setName = async () => {
+      const { value } = await Preferences.get({ key: 'userName' });
+      this.userName = value;
+    };
+    setName();
+
+    const setZone = async () => {
+      const { value } = await Preferences.get({ key: 'totalZones' });
+      this.zone = value;
+    };
+    setZone();
+  };
 }
+
+// ngOnDestroy() {
+//   this.subscription?.unsubscribe();
+// }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { ViewWillEnter } from '@ionic/angular';
+import { GetResult, Preferences } from '@capacitor/preferences';
+import { Router } from '@angular/router';
+import { stringify } from 'querystring';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements ViewWillEnter{
 
-  constructor() {}
+  formController!: FormGroup;
+  defaultTotalZones!:  string | null |GetResult;
+  defaultZoneChanges! : string | null;
+  defaultUserName! :  string | GetResult ;
 
+  constructor( private formBuilder: FormBuilder, private router: Router) {
+    this.ionViewWillEnter();
+    this.defaultUserName = 'mon petit marco';
+
+  }
+
+  ionViewWillEnter(): void {
+    this.formController = this.formBuilder.group({
+      userName: [this.defaultUserName, [Validators.required, Validators.maxLength(20)]],
+      totalZones: [this.defaultTotalZones, [Validators.required, Validators.pattern("^(0|[1-9][0-9]*)$")]],
+      zoneChanges: [this.defaultZoneChanges, [Validators.required, Validators.pattern("^(0|[1-9][0-9]*)$")]]
+    })
+  }
+
+  setByformValue = async (): Promise<void> =>{
+    const userName =this.formController.value.userName;
+    const totalZones =this.formController.value.totalZones ;
+    const zoneChanges =this.formController.value.zoneChanges ;
+
+    await Preferences.set({key:'userName',value:userName});
+    await Preferences.set({key:'totalZones',value:totalZones});
+    await Preferences.set({key:'zoneChanges',value:zoneChanges});
+  }
+
+  submitForm() {
+    if (this.formController.valid) {
+      console.log(this.formController.value);
+      this.setByformValue();
+      this.router.navigate(['tabs'])
+    }
+  }
+
+  savePreferences =  async () => {
+    if (this.formController.valid)  {
+
+      this.defaultUserName = await Preferences.get({ key: 'userName' }); 
+       
+      this.defaultTotalZones = await Preferences.get({ key: 'totalZones' }); 
+      this.router.navigate(['tabs']); 
+  }
+
+  
+}
 }
